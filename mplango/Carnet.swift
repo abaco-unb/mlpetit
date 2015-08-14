f0767f17dd63522b5b94249cbca6fd93143860e8
Thomas Petit <thomape18@hotmail.com>
2015-08-14 11:41:54 -0300

    ...

---
+++ mplango/Carnet.swift
@@ -0,0 +1,17 @@
+//
+//  Carnet.swift
+//  mplango
+//
+//  Created by Thomas Petit on 14/08/2015.
+//  Copyright (c) 2015 unb.br. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class Carnet: NSManagedObject {
+
+    @NSManaged var word: String
+    @NSManaged var desc: String
+
+}
