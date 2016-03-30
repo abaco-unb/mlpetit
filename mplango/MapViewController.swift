138eb1387bc1dab9e3c2dd61e92cf4a0a9d611b2
Bruno Ferreira <brunosfweb@gmail.com>
2016-03-30 17:34:31 -0300

    para guardar os dados anteriores

--- mplango/MapViewController.swift
+++ mplango/MapViewController.swift
@@ -13,13 +13,13 @@ import SwiftyJSON
 import CoreLocation
 
 
-class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate, UIPopoverPresentationControllerDelegate {
+class MapViewController: UIViewController, CLLocationManagerDelegate, UIPopoverPresentationControllerDelegate {
     
     @IBOutlet weak var mkMapView: MKMapView!
     
     let regionRadius: CLLocationDistance = 1000
     var locationManager = CLLocationManager()
-    var posts = [Annotation]()
+    var posts = [PostAnnotation]()
     
     var location:CLLocation!
     var street: String!
@@ -27,6 +27,15 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
     
     var restPath = "http://server.maplango.com.br/post-rest"
     var indicator:ActivityIndicator = ActivityIndicator()
+    
+    
+    var zoomLevel = Double()
+    var iphoneScaleFactorLatitude = Double()
+    var iphoneScaleFactorLongitude = Double()
+    var CanUpdateMap: Bool = false
+    var arrDicPostsWithLatitudeLongitude = [Dictionary<String, Double>]()
+    
+    let clusteringManager = FBClusteringManager()
     
     //Filtros: background e botões
     
@@ -40,6 +49,7 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
     @IBOutlet weak var showFiltersBtn: UIButton!
     @IBOutlet weak var hideFiltersBtn: UIButton!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     
@@ -50,8 +60,11 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
         let user:Int = prefs.integerForKey("id") as Int
         NSLog("usuário logado: %ld", user)
         
-        //recupera todos os posts e adiciona no mapa
-        self.upServerPosts()
+        
+        self.mkMapView.showsUserLocation = true
+        self.mkMapView.delegate = self
+        self.mkMapView.mapType = MKMapType.Standard
+        self.mkMapView.backgroundColor = UIColor.blueColor()
         
         //inicializa : get geo location data
         locationManager.requestAlwaysAuthorization()
@@ -63,11 +76,14 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
             NSLog("Serviço de localização indisponível")
         }
         
+        //recupera todos os posts e adiciona no mapa
+        self.upServerPosts()
+//        let numberOfLocations = 1000
+//        let array:[MKAnnotation] = randomLocationsWithCount(numberOfLocations)
         
-        self.mkMapView.showsUserLocation = true
-        self.mkMapView.delegate = self
-        self.mkMapView.mapType = MKMapType.Standard
-        self.mkMapView.backgroundColor = UIColor.blueColor()
+        
+        
+        
         
         //self.mkMapView.userTrackingMode = MKUserTrackingModeFollow;
         
@@ -107,32 +123,7 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
         
         //longPressRecogniser.minimumPressDuration = 1.0
         //mkMapView.addGestureRecognizer(longPressRecogniser)
-
-
         
-//        //MARK - retrieve all posts of Core Data
-//        let request = NSFetchRequest(entityName: "Post")
-//        if let posts = (try? moContext?.executeFetchRequest(request)) as? [Post] {
-//            
-//            if (posts.count > 0) {
-//                for post in posts {
-//                    //println(post)
-//                    var latDelta:CLLocationDegrees = 0.01
-//                    var longDelta:CLLocationDegrees = 0.01
-//                    
-//                    // show post on map
-//                    let annotation = Annotation(title: post.text,
-//                        locationName: post.locationName,
-//                        category: post.category,
-//                        coordinate: CLLocationCoordinate2D(latitude: post.latitude, longitude: post.longitude),
-//                        userImage: UIImage(data: (post.user as! User).image)!,
-//                        entity: post
-//                    )
-//                    
-//                    mkMapView.addAnnotation(annotation)
-//                }
-//            }
-//        }
     }
     
     //MARK: Actions
@@ -200,6 +191,7 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
             .responseSwiftyJSON({ (request, response, json, error) in
                 self.indicator.hideActivityIndicator();
                    if let posts = json["data"].array {
+                    
                         for post in posts {
                             var latitude:Double  = 0
                             var longitude:Double = 0
@@ -207,6 +199,7 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
                             
                             if let lat = post["latitude"].string {
                                 latitude = Double(lat)!
+                                
                             }
                             
                             if let long = post["longitude"].string {
@@ -219,7 +212,7 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
                             
                             print(latitude, longitude)
                              //show post on map
-                             let annotation = Annotation(
+                             let annotation = PostAnnotation(
                                 title: post["text"].stringValue,
                                 locationName: post["location"].stringValue,
                                 audio: post["audio"].stringValue,
@@ -227,9 +220,15 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
                                 coordinate: CLLocationCoordinate2D(latitude: latitude, longitude: longitude),
                                 userImage: post["photo"].stringValue
                              )
-                             self.mkMapView.addAnnotation(annotation)
+                             //self.arrDicPostsWithLatitudeLongitude.append(["latitude" : latitude, "longitude" : longitude])
+                            self.posts.append(annotation);
+                            //self.mkMapView.addAnnotation(annotation)
                         }
                    }
+                print( "total de posts : ",self.mkMapView.annotations.count)
+                   self.clusteringManager.addAnnotations(self.posts)
+                   self.clusteringManager.delegate = self;
+                
 //                    //print(json["data"].array?.count)
 //                   //print(json["data"].array?.count)
 //                    
@@ -249,7 +248,17 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
             });
         
     }
-
+    
+    func randomLocationsWithCount(count:Int) -> [FBAnnotation] {
+        var array:[FBAnnotation] = []
+        for _ in 0...count {
+            let a:FBAnnotation = FBAnnotation()
+            a.coordinate = CLLocationCoordinate2D(latitude: drand48() * 40 - 20, longitude: drand48() * 80 - 40 )
+            array.append(a)
+        }
+        return array
+    }
+    
     // MARK:- Retrieve Posts
     
     func checkLocationAuthorizationStatus() {
@@ -324,13 +333,19 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
 //            
 //        )
         
-        let regionToZone = MKCoordinateRegionMake(manager.location!.coordinate, MKCoordinateSpanMake(1,10))
-        
-        mkMapView.setRegion(regionToZone, animated: true)
         locationManager.stopUpdatingLocation();
         
+        let regionToZone = MKCoordinateRegionMake(manager.location!.coordinate, MKCoordinateSpanMake(1,10))
+        mkMapView.setRegion(regionToZone, animated: true)
+        
         centerMapOnLocation(userLocation)
+        
+        
         self.mkMapView.showAnnotations(self.mkMapView.annotations, animated: true)
+        
+        
+        //mkMapView.setVisibleMapRect(<#T##mapRect: MKMapRect##MKMapRect#>, edgePadding: <#T##UIEdgeInsets#>, animated: <#T##Bool#>)
+        
         
     }
     
@@ -392,8 +407,233 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
         return .None
     }
     
+//    
+//    func getLatitudeLongitudeLimitsFromMap(mapView: MKMapView) -> [String: Double] {
+//        var coord = [String: Double]()
+//        
+//        let MinLat: Double = mapView.region.center.latitude - (mapView.region.span.latitudeDelta / 2)
+//        let MaxLat: Double = mapView.region.center.latitude + (mapView.region.span.latitudeDelta / 2)
+//        let MinLon: Double = mapView.region.center.longitude - (mapView.region.span.longitudeDelta / 2)
+//        let MaxLon: Double = mapView.region.center.longitude + (mapView.region.span.longitudeDelta / 2)
+//        
+//        coord["MinLat"] = MinLat
+//        coord["MaxLat"] = MaxLat
+//        coord["MinLon"] = MinLon
+//        coord["MaxLon"] = MaxLon
+//        
+//        return coord
+//    }
     
+//    func LoadMap(mapView: MKMapView) {
+//        // Get the limits after move or resize the map
+//        let coord: [String: Double] = getLatitudeLongitudeLimitsFromMap(mapView)
+//        let MinLat: Double = coord["MinLat"]! as Double
+//        let MaxLat: Double = coord["MaxLat"]! as Double
+//        let MinLon: Double = coord["MinLon"]! as Double
+//        let MaxLon: Double = coord["MaxLon"]! as Double
+//        var arrAnnotations = [Annotation]()
+//        
+//        let FilterMinLat = self.posts.filter({
+//            if let item: Annotation = $0 {
+//                return item.coordinate.latitude > MinLat
+//            } else {
+//                return false
+//            }
+//        })
+//        
+//        let FilterMaxLat = FilterMinLat.filter({
+//            if let item : Annotation = $0 {
+//                return item.coordinate.latitude < MaxLat
+//            } else {
+//                return false
+//            }
+//        })
+//        let FilterMinLon = FilterMaxLat.filter({
+//            if let item : Annotation = $0 {
+//                return item.coordinate.longitude > MinLon
+//            } else {
+//                return false
+//            }
+//        })
+//        let FilterMaxLon = FilterMinLon.filter({
+//            if let item : Annotation = $0 {
+//                return item.coordinate.longitude < MaxLon
+//            } else {
+//                return false
+//            }
+//        })
+//        
+//        for post in FilterMaxLon {
+//            arrAnnotations.append(post)
+//        }
+//    
+//        // Show in the map only the annotations from that specific region
+//        iphoneScaleFactorLatitude = Double(mapView.bounds.size.width / 30)
+//        iphoneScaleFactorLongitude = Double(mapView.bounds.size.height / 30)
+//    
+//        if zoomLevel != mapView.region.span.longitudeDelta {
+//            filterAnnotations(mapView, arrAnnotations: arrAnnotations)
+//    
+//            zoomLevel = mapView.region.span.longitudeDelta
+//    
+//            CanUpdateMap = true
+//        }
+//    }
+//
+//    func filterAnnotations(mapView: MKMapView, arrAnnotations: [MKAnnotation]) {
+//        let latDelta: Double = mapView.region.span.longitudeDelta / iphoneScaleFactorLatitude
+//        let lonDelta: Double = mapView.region.span.longitudeDelta / iphoneScaleFactorLongitude
+//        var shopsToShow = [AnyObject]()
+//        var arrAnnotationsNew = [MKAnnotation]()
+//    
+//        for var i = 0; i < arrAnnotations.count; i++ {
+//            let checkingLocation: MKAnnotation = arrAnnotations[i]
+//            let latitude: Double = checkingLocation.coordinate.latitude
+//            let longitude: Double = checkingLocation.coordinate.longitude
+//            var found: Bool = false
+//        
+//            for tempPlacemark: MKAnnotation in shopsToShow as! [MKAnnotation] {
+//                if fabs(tempPlacemark.coordinate.latitude - latitude) < fabs(latDelta) && fabs(tempPlacemark.coordinate.longitude - longitude) < fabs(lonDelta) {
+//                    found = true
+//                }
+//            }
+//        
+//            if !found {
+//                shopsToShow.append(checkingLocation)
+//                arrAnnotationsNew.append(checkingLocation)
+//            }
+//        }
+//    
+//    
+//        // Clean the map
+//        for item: MKAnnotation in self.mkMapView.annotations {
+//            self.mkMapView.removeAnnotation(item)
+//        }
+//    
+//        // Add new annotations to the map
+//        for item: MKAnnotation in arrAnnotationsNew {
+//            self.mkMapView.addAnnotation(item)
+//        }
+//    }
     
+//    func mapView(mapView: MKMapView, regionDidChangeAnimated animated: Bool){
+//        NSOperationQueue().addOperationWithBlock({
+//            let mapBoundsWidth = Double(self.mkMapView.bounds.size.width)
+//            let mapRectWidth:Double = self.mkMapView.visibleMapRect.size.width
+//            let scale:Double = mapBoundsWidth / mapRectWidth
+//            let annotationArray = self.clusteringManager.clusteredAnnotationsWithinMapRect(self.mkMapView.visibleMapRect, withZoomScale:scale)
+//            self.clusteringManager.displayAnnotations(annotationArray, onMapView:self.mkMapView)
+//        })
+//    }
     
+//    func mapView(mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+//        if CanUpdateMap == true {
+//            LoadMap(mapView)
+//        }
+//    }
+
+//    func zoomToFitMapAnnotations(aMapView: MKMapView) {
+//        
+//        if aMapView.annotations.count == 0 {
+//            
+//            return
+//        
+//        }
+//        
+//        var topLeftCoord: CLLocationCoordinate2D = CLLocationCoordinate2D()
+//        topLeftCoord.latitude = -90
+//        topLeftCoord.longitude = 180
+//        
+//        var bottomRightCoord: CLLocationCoordinate2D = CLLocationCoordinate2D()
+//        bottomRightCoord.latitude = 90
+//        bottomRightCoord.longitude = -180
+//        
+//        for annotation: MKAnnotation in self.mkMapView.annotations{
+//            topLeftCoord.longitude = fmin(topLeftCoord.longitude, annotation.coordinate.longitude)
+//            topLeftCoord.latitude = fmax(topLeftCoord.latitude, annotation.coordinate.latitude)
+//            bottomRightCoord.longitude = fmax(bottomRightCoord.longitude, annotation.coordinate.longitude)
+//            bottomRightCoord.latitude = fmin(bottomRightCoord.latitude, annotation.coordinate.latitude)
+//        }
+//        
+//        var region: MKCoordinateRegion = MKCoordinateRegion()
+//        region.center.latitude = topLeftCoord.latitude - (topLeftCoord.latitude - bottomRightCoord.latitude) * 0.5
+//        region.center.longitude = topLeftCoord.longitude + (bottomRightCoord.longitude - topLeftCoord.longitude) * 0.5
+//        region.span.latitudeDelta = fabs(topLeftCoord.latitude - bottomRightCoord.latitude) * 1.4
+//        region.span.longitudeDelta = fabs(bottomRightCoord.longitude - topLeftCoord.longitude) * 1.4
+//        region = aMapView.regionThatFits(region)
+//        
+//        self.mkMapView.setRegion(region, animated: true)
+//    }
+}
+
+extension MapViewController : FBClusteringManagerDelegate {
+    
+    func cellSizeFactorForCoordinator(coordinator:FBClusteringManager) -> CGFloat{
+        return 1.0
+    }
+    
+}
+
+
+extension MapViewController : MKMapViewDelegate {
+    
+    func mapView(mapView: MKMapView, regionDidChangeAnimated animated: Bool){
+        
+        NSOperationQueue().addOperationWithBlock({
+            
+            let mapBoundsWidth = Double(self.mkMapView.bounds.size.width)
+            
+            let mapRectWidth:Double = self.mkMapView.visibleMapRect.size.width
+            
+            let scale:Double = mapBoundsWidth / mapRectWidth
+            
+            let annotationArray = self.clusteringManager.clusteredAnnotationsWithinMapRect(self.mkMapView.visibleMapRect, withZoomScale:scale)
+            
+            self.clusteringManager.displayAnnotations(annotationArray, onMapView:self.mkMapView)
+            
+        })
+        
+    }
+    
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        var reuseId = ""
+
+        if annotation.isKindOfClass(FBAnnotationCluster) {
+            
+            reuseId = "Cluster"
+            var clusterView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId)
+            clusterView = FBAnnotationClusterView(annotation: annotation, reuseIdentifier: reuseId, options: nil)
+            
+            return clusterView
+            
+        }
+        
+        if annotation.isKindOfClass(PostAnnotation) {
+            
+            print( "titulo : ", annotation.title)
+            
+            reuseId = "Post"
+            var postView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId)
+            postView = MKAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            postView!.canShowCallout = true
+            postView!.image = UIImage(named: String((annotation as! PostAnnotation).getCategoryImageName()))
+            postView!.backgroundColor = UIColor.clearColor()
+            
+            postView!.calloutOffset = CGPoint(x: -5, y: 5)
+            //              postView.rightCalloutAccessoryView = postButton
+            
+            let imageview = UIImageView(frame: CGRectMake(0, 0, 45, 45))
+//            let imgUtils:ImageUtils = ImageUtils()
+//            let image: UIImage = imgUtils.loadImageFromPath((annotation as! PostAnnotation).userImage)!
+//            imageview.image = image
+            
+            postView!.leftCalloutAccessoryView = imageview
+            
+            return postView
+        }
+        
+        return nil
+    }
     
 }
